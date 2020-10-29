5a532409d9f02e5ea3260fe483062026db0ff994
slowbrain-jaym <jamenator94@gmail.com>
2020-10-29 13:57:05 +0000

    Added htc values to body model, added on off power control, added food setpoint and save to csv functionality

--- TraditionalOvenModel.py
+++ TraditionalOvenModel.py
@@ -1,21 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from BodyModelling import *
 
 rockwool = RockWool()
 steel = Steel()
 air = Air()
-hipor = Hipor()
+chicken = Chicken()
 door = Door()
 
 bodies = {}
-bodies['fanair'] = Gas(290, 0.002, air, 50, 0.1)
-bodies['ovenair'] = Gas(290, 0.05, air, 5, 0.25)
+bodies['fanair'] = Gas(290, 0.002, air, 25)
+bodies['ovenair'] = Gas(290, 0.05, air, 15)
 bodies['fanshroud'] = Solid(290, 0.00006, steel, 0.001)
 bodies['ovenback'] = Solid(290, 0.00012, steel, 0.001)
 bodies['ovenbackinsulation'] = Solid(290, 0.0024, rockwool, 0.02)
 bodies['ovenbackouter'] = Solid(290, 0.00012, steel, 0.001)
-bodies['food'] = Solid(290, 0.0036, hipor, 5)
+bodies['food'] = Solid(277, 0.0036, chicken, 0.05)
 bodies['ovenwalls'] = Solid(290, 0.00072, steel, 0.001)
 bodies['oveninsulation'] = Solid(290, 0.0144, rockwool, 0.02)
 bodies['ovenouter'] = Solid(290, 0.00072, steel, 0.001)
@@ -44,27 +45,43 @@ exchangers.append(HeatExchange(bodies['ovendoor'], bodies['outsideair'], 0.2))
 
 
 step_time = 0.1
-steps = 6000
+steps = 90000
 step_count = 0
+setpoint = 180+273
+foodtarget = 74+273
+power = 2400 #Watts
+savename = "Traditional Oven Chicken"
 results = {}
+results["time"] = []
+results["energy"] = []
 for body in bodies:
     results[body] = []
 
 
-while step_count<steps:
+while step_count<steps and bodies["food"].temp<foodtarget:
     #print "step: ", step_count
-    bodies["fanair"].add_flux(2400)
+    if(bodies['ovenair'].temp<setpoint):
+        bodies["fanair"].add_flux(2400)
+        results["energy"].append(2400*step_time)
+    else:
+        results["energy"].append(0)
     for exchanger in exchangers:
         exchanger.calc_flux()
     for body in bodies:
         results[body].append(bodies[body].temp-273)
         bodies[body].time_step(step_time)        
         #print body, bodies[body].temp
+    results["time"].append(step_time*step_count)
     step_count+=1
 
+print("Cook complete at:",results["time"][-1]/60, "mins")
+print("Energy consumption:",sum(results["energy"])/1000/3600,"kWhr")
 for body in bodies:
     print(body, results[body][-1])
-    plt.plot(np.linspace(0,step_time*steps/60.0,steps),results[body],label=body)
+    plt.plot(results["time"],results[body],label=body)
+
+results_df = pd.DataFrame(results)
+results_df.to_csv(savename+".csv")
 
 plt.legend()
 plt.show()
